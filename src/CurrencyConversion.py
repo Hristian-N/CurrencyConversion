114f2f1d64a7e33ef753327de6ee35a224678a2d
Hristian-N <hristian.kr.nikolov@gmail.com>
2024-05-25 15:12:31 +0300

    Added saving to json functionality,caching

--- src/CurrencyConversion.py
+++ src/CurrencyConversion.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from datetime import datetime
 from decimal import Decimal, InvalidOperation
 import requests
@@ -11,6 +12,8 @@ def load_api_key(config_file='config.json'):
 
 API_KEY = load_api_key()
 BASE_URL = 'https://api.fastforex.io'
+cache = {}
+json_filename = 'results.json'
 
 def validate_date(date_str):
     try:
@@ -41,7 +44,7 @@ def get_currencies(currency_code):
 
 def is_currency_present():
     while True:
-        currency_code = input("Enter currency code: ").upper()
+        currency_code = input("").upper()
 
         if currency_code == "END":
             exit()
@@ -52,30 +55,45 @@ def is_currency_present():
         else:
             print("Please enter a valid currency code")
 
-def convert(base_currency, target_currencies):
+def convert(base_currency_code, target_currency_code):
+    if base_currency_code in cache and target_currency_code in cache[base_currency_code]['results']:
+        print(f"Using cached data for {base_currency_code} to {target_currency_code}")
+        print(cache)
+        return cache[base_currency_code]
 
     endpoint = f'{BASE_URL}/time-series'
 
     params = {
-        'from': base_currency,
-        'to': target_currencies,
+        'from': base_currency_code,
+        'to': target_currency_code,
         'interval': 'P1D',
         'api_key': API_KEY
     }
 
     response = requests.get(endpoint, params=params)
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        # Merge with existing data if base currency is already in cache
+        if base_currency_code in cache:
+            existing_data = cache[base_currency_code]
+            for key, value in data['results'].items():
+                if key in existing_data['results']:
+                    existing_data['results'][key].update(value)
+                else:
+                    existing_data['results'][key] = value
+        else:
+            cache[base_currency_code] = data
+        return data
     else:
         return response.text
 
 
 def find_value_on_date(date, target_currency_code, data):
+
     results = data['results']
     dates = results[target_currency_code.upper()]
 
     if date in dates:
-        print(dates[date])
         return dates[date]
 
     for key, value in dates.items():
@@ -86,13 +104,34 @@ def find_value_on_date(date, target_currency_code, data):
     return None
 
 
+def save_to_json(base_currency, target_currency, amount, converted_amount, date):
+    log = {
+        "base_currency": base_currency,
+        "target_currency": target_currency,
+        "amount": amount,
+        "converted_amount": converted_amount,
+        "date": date.strftime('%Y-%m-%d')
+    }
+
+    if os.path.exists(json_filename):
+        with open(json_filename, 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+
+    data.append(log)
+
+    with open(json_filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
 def main(args):
 
     while True:
 
         # Amount
         while True:
-            amount = input("Amount: ")
+            amount = input("")
 
             if amount.upper() == "END":
                 exit()
@@ -107,11 +146,16 @@ def main(args):
         # Target Currency
         target_currency_code = is_currency_present()
 
-        # Find value for the chosen date
-        value = find_value_on_date(args.date, target_currency_code, convert(base_currency_code, target_currency_code))
+        json_data = convert(base_currency_code, target_currency_code)
+
+        value = find_value_on_date(args.date, target_currency_code, json_data)
+
         # calculate
-        print(round(float(amount) * value, 2))
+        result = str(amount) + " " + base_currency_code.upper() + " is " + str(
+            round(float(amount) * value, 2)) + " " + target_currency_code.upper()
+        print(result)
         print()
+        save_to_json(base_currency_code, target_currency_code, amount, result, args.date)
 
 
 if __name__ == "__main__":
